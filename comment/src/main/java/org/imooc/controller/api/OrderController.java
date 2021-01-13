7dbe02902ddc911909c845ccee6b6d5eb4096455
weibgg <weibgg@163.com>
2021-01-13 16:22:44 +0800

    add order code

--- comment/src/main/java/org/imooc/controller/api/OrderController.java
+++ comment/src/main/java/org/imooc/controller/api/OrderController.java
@@ -3,9 +3,12 @@ package org.imooc.controller.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.support.JstlUtils;
 
+/**
+ * @author Administrator
+ */
 @Controller
 public class OrderController {
     public void selectOrder(){
-        System.out.println("team2 开发 订单 第一天");
+        System.out.println("team2开发订单第一天");
     }
 }
